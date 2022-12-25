be974c4c9aa518e2c3d1b330e653b15ed5bcee09
TB-210411 <98394970+TB-210411@users.noreply.github.com>
2022-12-25 16:40:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+prediction1="";
+prediction2="";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach(camera);
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image"src="'+data_uri+'"/>';
+    });
+}
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Hh06zgW-f/model.json",modelLoaded);
+function modelLoaded(){
+    console.log("Model Loaded!!");
+}
